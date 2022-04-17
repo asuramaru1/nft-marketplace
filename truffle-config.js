fef6e3f052ddbc328808824909d088bd06ce8a56
Givi Khartishvili <gkhar17@freeuni.edu.ge>
2022-04-17 21:24:57 +0400

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  networks: {
+    development: {
+     host: process.env.TRUFFLE_DEV_HOST,
+     port: process.env.TRUFFLE_DEV_PORT,
+     network_id: process.env.TRUFFLE_DEV_NETWORK_ID,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    }
+  },
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
+};
